9fe531af70678788d3350c27f7505d4668dc8eec
agrela <agrela2000@hotmail.com>
2017-06-26 14:08:42 +0200

    Add files via upload
    ESP\
    Ejemplo ESP8266 en el formato WEMOS D1 MINI\
    Empleando el shield DHT11 / DHT22\
    Conectando el pin D0 con el RST realiza una medida cada 30 min. aprox.\
    Muy sencillo de utilizar, simplemente hay que añadir los datos de nuestra wifi y los que aporta la web de Cayenne.\
    \
    ENG\
    Using shield DHT11 / DHT22\
    You must connect D0 and RST, that way the board acquire temperature and humidity every 30 min.\
    Very easy to use, you only have to add your wifi data and the Cayenne web data.\

---
+++ wemos_d1_mini_dht11.ino
@@ -0,0 +1,43 @@
+//Ejemplo de uso DHT11 con wemos D1 mini
+//Libraries
+#include <DHT.h>;
+#include <ESP8266WiFi.h>
+#include <CayenneMQTTESP8266.h>;
+
+//Constants
+#define CAYENNE_PRINT Serial
+#include <CayenneMQTTESP8266.h>
+#define DHTPIN D4         // * pin usado DHT11 en wemos D1 mini shield
+#define DHTTYPE DHT11      // o DHT22
+DHT dht(DHTPIN, DHTTYPE);   // Initialize DHT sensor for normal 16mhz Arduino
+
+// WiFi network info.
+char ssid[] = "ssid";    
+char wifiPassword[] = "pass"; 
+
+// Cayenne authentication info. This should be obtained from the Cayenne Dashboard.
+char username[] = "MQTT Usernane";
+char password[] = "MQTT Password";
+char clientID[] = "Client ID";
+
+unsigned long lastMillis = 0;
+
+void setup() {
+  Serial.begin(9600);
+  Cayenne.begin(username, password, clientID, ssid, wifiPassword);
+  dht.begin();
+}
+
+//Variables
+float hum;  //Stores humidity value
+float temp; //Stores temperature value
+
+void loop(){
+    Cayenne.loop();
+    delay(2000);
+    //Read data and store it to variables hum and temp
+    hum = dht.readHumidity();
+    temp= dht.readTemperature();
+    Cayenne.celsiusWrite(2, temp);
+    Cayenne.virtualWrite(1, hum);
+}
